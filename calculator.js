1b5d6af8dd24707da500ea1bab86c40469738960
Federico Gomez Conde <fedeic8@gmail.com>
2023-06-28 16:36:25 -0300

    feat: se agrega caso de divison por cero

--- calculator.js
+++ calculator.js
@@ -1,53 +1,55 @@
-// Evaluar por orden de precedencia -> 1ero multiplicación y división y luego suma y resta
-// Cambiar la validación por test, ya que debo obtener los grupos de operaciones que tenga todo el texto. O formatear las partes para poder usar ese código usando template literals
-// testcadena = `{sumar} {num1} y {num2}`
-// Primero evaluar match por cada operación y guardar en una variable y luego realizar el condicional para hacer las operaciones
+/* CALCULADORA QUE FUNCIONA INGRESANDO UN TEXTO CON LAS OPERACIONES DESEADAS
+Se utilzan expresiones regulares (REGEX) para identificar y resolver las operaciones */
 
-
-function realizarOperaciones(cadena) {
+function calculadora(cadena) {
   const regexOperacion = /(suma|sumar|resta|restar|division|dividir|multiplicacion|multiplicar)/i;
-  const regexNumeros = /(\d+)\D+(\d+)/i;
-
-  let result1 = null;
+  const regexNumeros = /(\d+)\D+(\d+)/i;   
+  let result = null;
 
   // Paso 1: Buscar y realizar la primera operación matemática
-  const matchesOperacion = cadena.match(regexOperacion);
+  const matchesOperacion = cadena.match(regexOperacion);       //devuelve un array con la primer operación encontrada en posición [0]
   if (matchesOperacion) {
     const operacion = matchesOperacion[0];
-    const matchesNumeros = cadena.match(regexNumeros);
+    const matchesNumeros = cadena.match(regexNumeros);        // devuelve un array con la coincidencia en posición [0] y los grupos en las posiciones siguientes [1] y [2]
 
     if (matchesNumeros) {
       const num1 = parseInt(matchesNumeros[1]);
       const num2 = parseInt(matchesNumeros[2]);
 
       if (/suma|sumar/i.test(operacion)) {
-        result1 = num1 + num2;
+        result = num1 + num2;
       } else if (/resta|restar/i.test(operacion)) {
-        result1 = num1 - num2;
+        result = num1 - num2;
       } else if (/division|dividir/i.test(operacion)) {
-        result1 = num1 / num2;
+        if (num2 !== 0){
+          result = num1 / num2;
+        } else { result = 'error'}
       } else if (/multiplicacion|multiplicar/i.test(operacion)) {
-        result1 = num1 * num2;
+        result = num1 * num2;
       }
 
       // Paso 2: Crear nueva cadena eliminando la operación y los números encontrados previamente
-      cadena = cadena.replace(matchesOperacion[0], "").replace(matchesNumeros[0], "");
+      cadenaNueva = cadena.replace(matchesOperacion[0], "").replace(matchesNumeros[0], "");
       // console.log(cadena);
     }
+  } else {
+    result = 'No se encontraron operaciones matemáticas a realizar, intente de nuevo'
   }
 
   // Paso 3: Buscar y realizar la siguiente operación matemática (si existe)
   while (true) {
-    const matchesOperacion = cadena.match(regexOperacion);
-    console.log(matchesOperacion);
-    const regexNumero = /(\d+)/
-
-    if (!matchesOperacion) {
+    if (result === 'error'){
       break;
     }
 
+    const matchesOperacion = cadenaNueva.match(regexOperacion);
+    
+    if (!matchesOperacion) {
+      break;
+    }
+    const regexNumero = /(\d+)/
     const operacion = matchesOperacion[0];
-    const matchesNumeros = cadena.match(regexNumero);
+    const matchesNumeros = cadenaNueva.match(regexNumero);
     // console.log(matchesNumeros);
 
     if (!matchesNumeros) {
@@ -57,22 +59,24 @@ function realizarOperaciones(cadena) {
     const numero = parseInt(matchesNumeros[1]);
 
     if (/suma|sumar/i.test(operacion)) {
-      result1 += numero;
+      result += numero;
     } else if (/resta|restar/i.test(operacion)) {
-      result1 -= numero;
+      result -= numero;
     } else if (/division|dividir/i.test(operacion)) {
-      result1 /= numero;
+      if (numero !== 0){
+        result /= numero;
+      } else { result = 'error'}
     } else if (/multiplicacion|multiplicar/i.test(operacion)) {
-      result1 *= numero;
+      result *= numero;
     }
 
-    cadena = cadena.replace(matchesOperacion[0], "").replace(matchesNumeros[0], "");
+    cadenaNueva = cadenaNueva.replace(matchesOperacion[0], "").replace(matchesNumeros[0], "");
   }
 
-  return result1 || "No se encontró ninguna operación matemática.";
+  return result || "No se encontró ninguna operación matemática.";
 }
 
 // Ejemplo de uso
-const cadena = "Hacer la resta de 10 y 20 y multiplicar por 2 y sumar 5";
-const resultado = realizarOperaciones(cadena);
+const cadena = "Hacer la suma de 50 y 20 y multiplicar por 2 y dividir por 5, luego restar 2";
+const resultado = calculadora(cadena);
 console.log(resultado); 
